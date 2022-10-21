4201089aa4f45f0c8a877dba48d04218c058326d
Mitja <mitja.immonen@hotmail.com>
2022-10-21 22:20:56 +0300

    portfolio data population & css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './experience.css'
-
+import { BsPatchCheckFill } from 'react-icons/bs'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Novice</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>C#</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Shit</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Nope</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+              <h4>PHP</h4>
+              <small className='text-light'>Noob</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
 
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/space.jpg'
+import IMG2 from '../../assets/slash.jpg'
+import IMG3 from '../../assets/splash.png'
+import IMG4 from '../../assets/cow.jpg'
+import IMG5 from '../../assets/abra.jpg'
+import IMG6 from '../../assets/space.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Game Jam Project',
+    github: 'https://github.com',
+    demo: 'https://itch.io'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} rel="noreferrer" className='btn' target='_blank'>GitHub</a>
+                  <a href={demo} rel="noreferrer" className='btn' target='_blank'>Itch</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
